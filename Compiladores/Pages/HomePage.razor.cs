228510db662d22f1cabf61493bf6011c02289abf
AdrianSibajaRetana <adriansretana@hotmail.com>
2021-05-16 21:19:16 -0600

    Método de prueba creado
    - Se creó un método de prueba para el botón de la página de compilación.
    - El método de prueba se encarga de compilar la entrada escrita por el usuario. Si la entrada es "Entrada" da estado de compilado e imprime la salida "Salida", de ser diferente da estado de error e imprime la salida vacía.

---
+++ Compiladores/Pages/HomePage.razor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Compiladores.Pages
+{
+    public partial class HomePage
+    {
+        public string EntradaDeUsuario { get; set; }
+
+        public string Salida { get; set; }
+
+        public string Estado { get; set; }
+
+        private void CompileEntry()
+        {
+            if (EntradaDeUsuario == "Entrada")
+            {
+                Salida = "Salida";
+                Estado = "Compilado";
+            }
+            else 
+            {
+                Salida = "";
+                Estado = "Error";
+            }
+        }
+    }
+}
